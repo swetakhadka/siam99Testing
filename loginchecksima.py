f2260902c0c968ca0dfbb5543c37224cb017fe59
Sweta Khadka <151756848+swetakhadka@users.noreply.github.com>
2024-10-02 12:03:49 +0545

    Update and rename siam.py to loginchecksima.py

--- loginchecksima.py
+++ loginchecksima.py
@@ -80,49 +80,49 @@ for index, banner in enumerate(banners):
 
 # Close the browser after finishing
 driver.quit()
-#  def login():
-#      login_button = driver.find_element(By.XPATH, "/html[1]/body[1]/div[1]/div[1]/div[1]/header[1]/div[1]/div[1]/a[1]")
-#      login_button.click()
+  def login():
+     login_button = driver.find_element(By.XPATH, "/html[1]/body[1]/div[1]/div[1]/div[1]/header[1]/div[1]/div[1]/a[1]")
+     login_button.click()
 #
-#      time.sleep(2)
+     time.sleep(2)
 #
 #
-#     username_button = driver.find_element(By.XPATH, " //input[@id='nK7dDJpdOWE_8']")
-#     submit_button = driver.find_element(By.XPATH, "//button[@type='submit']")
-#     password_button = driver.find_element(By.XPATH, " //input[@id='nK7dDJpdOWE_9']")
+     username_button = driver.find_element(By.XPATH, " //input[@id='nK7dDJpdOWE_8']")
+    submit_button = driver.find_element(By.XPATH, "//button[@type='submit']")
+    password_button = driver.find_element(By.XPATH, " //input[@id='nK7dDJpdOWE_9']")
 #
-#     usernameArr = ["user123", "admin", "bishwa", "thb0092", "thb001"]
-#     passwordArr = ["user123", "admin", "bishwa", "thb0092", "Welcome1"]
+     usernameArr = ["user123", "admin", "bishwa", "thb0092", "thb001"]
+    passwordArr = ["user123", "admin", "bishwa", "thb0092", "Welcome1"]
 #
-#     for username, password in zip(usernameArr, passwordArr):
-#         username_button.click()
-#         username_button.send_keys(Keys.CONTROL + "a")
-#         username_button.send_keys(Keys.DELETE)
-#         time.sleep(1)
-#         password_button.click()
-#         password_button.send_keys(Keys.CONTROL + "a")  # Select all text
-#         password_button.send_keys(Keys.DELETE)
+     for username, password in zip(usernameArr, passwordArr):
+        username_button.click()
+        username_button.send_keys(Keys.CONTROL + "a")
+         username_button.send_keys(Keys.DELETE)
+         time.sleep(1)
+         password_button.click()
+         password_button.send_keys(Keys.CONTROL + "a")  # Select all text
+         password_button.send_keys(Keys.DELETE)
 #
 #         # Input the username and password
-#         username_button.send_keys(username)
-#         time.sleep(0.1)  # Short delay to mimic human typing
-#         password_button.send_keys(password)
-#         time.sleep(2)
+        username_button.send_keys(username)
+        time.sleep(0.1)  # Short delay to mimic human typing
+        password_button.send_keys(password)
+         time.sleep(2)
 #
-#         submit_button.click()
+         submit_button.click()
 #
-#         time.sleep(1)
+         time.sleep(1)
 #
-# login()
+ login()
 
-# try:
-#     home_page_element = WebDriverWait(driver, 20).until(
-#         EC.presence_of_element_located((By.XPATH, "//button[@class='focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 flex-shrink-0 min-w-//span[@class='text-[14px] md:text-[1rem]'][normalize-space()='13,045.00']"))
-#     )
-#     print("Successfully navigated to the home page!")
-# except Exception as e:
-#     print("Failed to navigate to the home page:", e)
-# # Close the browser
-# driver.implicitly_wait(10)
+ try:
+    home_page_element = WebDriverWait(driver, 20).until(
+         EC.presence_of_element_located((By.XPATH, "//button[@class='focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 flex-shrink-0 min-w-//span[@class='text-[14px] md:text-[1rem]'][normalize-space()='13,045.00']"))
+    
+    print("Successfully navigated to the home page!")
+ except Exception as e:
+    print("Failed to navigate to the home page:", e)
+# Close the browser
+ driver.implicitly_wait(10)
 driver.quit()
 
